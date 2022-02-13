c632e33dc36d1b3398ecc30bb38961a4bc5ef4ea
Kostiantyn Chorobuk <kostiantyn.chornobuk@gmail.com>
2022-02-13 12:32:38 +0200

    updated filter and web.xml for suporting cyrillic symbols

--- src/main/java/com/chornobuk/web/controller/filter/EncodingFilter.java
+++ src/main/java/com/chornobuk/web/controller/filter/EncodingFilter.java
@@ -4,23 +4,24 @@ import javax.servlet.*;
 import java.io.IOException;
 
 public class EncodingFilter implements Filter {
-//	@Override
-//	public boolean isLoggable(LogRecord record) {
-//		return false;
-//	}
+	private String encoding;
 
 	public void destroy() {
 //		do nothing
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		chain.doFilter(request, response);
+		String requestEncoding = request.getCharacterEncoding();
+		if(requestEncoding == null) {
+			request.setCharacterEncoding(encoding);
+		}
+		chain.doFilter(request,response);
 	}
 
-//	@Override
-//	public void init(FilterConfig filterConfig) throws ServletException {
-//		Filter.super.init(filterConfig);
-//	}
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		encoding = filterConfig.getInitParameter("encoding");
+
+	}
 
 }
