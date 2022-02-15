acb16e35265dc24d081087fe06d4498b7392e89a
Kostiantyn Chorobuk <kostiantyn.chornobuk@gmail.com>
2022-02-15 10:11:00 +0200

    added logout command

--- src/main/java/com/chornobuk/web/controller/command/LogoutCommand.java
+++ src/main/java/com/chornobuk/web/controller/command/LogoutCommand.java
@@ -2,19 +2,24 @@ package com.chornobuk.web.controller.command;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class LogoutCommand implements ICommand{
 	@Override
 	public String execute(HttpServletRequest req, HttpServletResponse resp) {
-		String forward = "redirect";
-		req.getSession().removeAttribute("user");
-		req.getSession().removeAttribute("role");
-		try {
-			resp.sendRedirect("index.jsp");
-		} catch (IOException e) {
-			e.printStackTrace();
+		String forward = "login.jsp";
+		HttpSession session = req.getSession();
+		if(session != null) {
+			session.invalidate();
 		}
+//		req.getSession().removeAttribute("user");
+//		req.getSession().removeAttribute("role");
+//		try {
+//			resp.sendRedirect("login.jsp");
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 		return forward;
 	}
 }
