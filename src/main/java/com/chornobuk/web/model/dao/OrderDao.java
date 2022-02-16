c0369dfcf3c64ed73e93c3db1094046b12c13d8e
Kostiantyn Chorobuk <kostiantyn.chornobuk@gmail.com>
2022-02-16 19:06:06 +0200

    update daos for tickets and orders

--- src/main/java/com/chornobuk/web/model/dao/OrderDao.java
+++ src/main/java/com/chornobuk/web/model/dao/OrderDao.java
@@ -1,7 +1,10 @@
 package com.chornobuk.web.model.dao;
 
 import com.chornobuk.web.model.database.DBManager;
+import com.chornobuk.web.model.entity.MovieSession;
 import com.chornobuk.web.model.entity.Order;
+import com.chornobuk.web.model.entity.Ticket;
+import com.chornobuk.web.model.entity.User;
 
 import java.sql.*;
 import java.time.LocalDateTime;
@@ -11,6 +14,7 @@ import java.util.Stack;
 public class OrderDao implements IDao<Order> {
 	private final static String GET_ORDER_BY_ID = "select * from tickets_order where order_id = ?";
 	private final static String INSERT_ORDER = "insert into tickets_order values(default,?,?,?)";
+	private final static String INSERT_TICKET = "insert into ticket values(default, ?, ?, ?)";
 
 	@Override
 	public Order get(long id) {
@@ -42,7 +46,7 @@ public class OrderDao implements IDao<Order> {
 			setParams(ps, order);
 			ps.execute();
 			ResultSet rs = ps.getGeneratedKeys();
-			if(rs.next())
+			if (rs.next())
 				order.setId(rs.getLong(1));
 			con.close();
 		} catch (SQLException e) {
@@ -58,6 +62,47 @@ public class OrderDao implements IDao<Order> {
 	@Override
 	public void delete(Order order) {
 //		don't need now
+	}
+
+	public void addOrderWithTickets(Order order, User user, MovieSession session, Ticket[] tickets) {
+		Connection con = DBManager.getInstance().getConnection();
+		try {
+			PreparedStatement insertOder = con.prepareStatement(INSERT_ORDER, Statement.RETURN_GENERATED_KEYS);
+			PreparedStatement insertTicket = con.prepareStatement(INSERT_TICKET, Statement.RETURN_GENERATED_KEYS);
+			con.setAutoCommit(false);
+//			set parameters for order
+			insertOder.setLong(1, user.getId());
+			insertOder.setLong(2, 2);
+			insertOder.setObject(3, order.getCreationDate());
+			insertOder.execute();
+			ResultSet orderKey = insertOder.getGeneratedKeys();
+			if (orderKey.next())
+				order.setId(orderKey.getLong(1));
+			for (int i = 0; i < tickets.length; i++) {
+//			set parameters for ticket
+				insertTicket.setInt(1, tickets[i].getPlaceNumber());
+				insertTicket.setLong(2, order.getId());
+				insertTicket.setLong(3, session.getId());
+				insertTicket.execute();
+				ResultSet ticketKey = insertTicket.getGeneratedKeys();
+				if (ticketKey.next())
+					tickets[i].setId(ticketKey.getLong(1));
+			}
+			con.commit();
+//			commit transaction
+			con.setAutoCommit(true);
+			con.close();
+		} catch (SQLException e) {
+			if (con != null) {
+				try {
+					System.out.println("transaction is being rolled back");
+					con.rollback();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+				e.printStackTrace();
+			}
+		}
 	}
 
 	private void setParams(PreparedStatement ps, Order order) throws SQLException {
--- src/main/java/com/chornobuk/web/model/dao/TicketDao.java
+++ src/main/java/com/chornobuk/web/model/dao/TicketDao.java
@@ -1,14 +1,17 @@
 package com.chornobuk.web.model.dao;
 
 import com.chornobuk.web.model.database.DBManager;
+import com.chornobuk.web.model.entity.MovieSession;
 import com.chornobuk.web.model.entity.Ticket;
 
 import java.sql.*;
+import java.util.LinkedList;
 import java.util.List;
 
 public class TicketDao implements IDao<Ticket>{
 	private final static String GET_TICKET_BY_ID = "select * from ticket where ticket_id = ?";
 	private final static String INSERT_TICKET = "insert into ticket values (default ,?,?,?)";
+	private final static String GET_TICKETS_BY_MOVIE = "select * from ticket where movie_session_id = ?";
 	@Override
 	public Ticket get(long id) {
 		Ticket ticket = null;
@@ -56,6 +59,23 @@ public class TicketDao implements IDao<Ticket>{
 	@Override
 	public void delete(Ticket ticket) {
 //		don't need now
+	}
+
+	public List<Ticket> getTicketsBySession(MovieSession session) {
+		List<Ticket> tickets = new LinkedList<>();
+		try{
+			Connection con = DBManager.getInstance().getConnection();
+			PreparedStatement ps = con.prepareStatement(GET_TICKETS_BY_MOVIE);
+			ps.setLong(1,session.getId());
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				tickets.add(getTicket(rs));
+			}
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return tickets;
 	}
 
 	private void setParams(PreparedStatement ps, Ticket ticket) throws SQLException{
