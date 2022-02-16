6bca05e4d242801e64cad4e7aef7c498eabc8fab
Kostiantyn Chorobuk <kostiantyn.chornobuk@gmail.com>
2022-02-16 23:43:13 +0200

    created profie page and showing profile command

---
+++ src/main/java/com/chornobuk/web/controller/command/ShowProfileCommand.java
@@ -0,0 +1,26 @@
+package com.chornobuk.web.controller.command;
+
+import com.chornobuk.web.model.dao.OrderDao;
+import com.chornobuk.web.model.entity.User;
+import com.chornobuk.web.model.entity.UserRole;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class ShowProfileCommand implements ICommand {
+	@Override
+	public String execute(HttpServletRequest req, HttpServletResponse resp) {
+		String redirect = "WEB-INF/jsp/profile.jsp";
+		OrderDao orderDao = new OrderDao();
+		UserRole role = (UserRole) req.getSession().getAttribute("role");
+
+		if (role == UserRole.USER) {
+			User user = (User) req.getSession().getAttribute("user");
+			HashMap<Long, Integer> orderIdPriceMap = new HashMap<>(orderDao.getOrdersPricesByUser(user));
+			req.setAttribute("ordersPrices", orderIdPriceMap);
+		}
+		return redirect;
+	}
+}
