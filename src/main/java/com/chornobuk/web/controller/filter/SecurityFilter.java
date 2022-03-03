11493a842d059fbda113507d4215a0d9409cbea1
Kostiantyn Chorobuk <kostiantyn.chornobuk@gmail.com>
2022-03-03 22:52:52 +0200

    updated security filter (added different error pages)

--- src/main/java/com/chornobuk/web/controller/filter/SecurityFilter.java
+++ src/main/java/com/chornobuk/web/controller/filter/SecurityFilter.java
@@ -4,6 +4,7 @@ import com.chornobuk.web.model.entity.UserRole;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.*;
@@ -13,13 +14,44 @@ public class SecurityFilter implements Filter {
 	private static List<String> common = new LinkedList<>();
 	private static Map<UserRole, List<String>> accessMap = new HashMap<>();
 	private static List<String> outOfControl = new LinkedList<>();
+	private int error;
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		if (isAllowed(request)) {
+		if (isActionAllowed(request)) {
 			chain.doFilter(request, response);
 		} else {
-			request.getRequestDispatcher("/403.jsp").forward(request, response);
+			HttpServletResponse httpResponse = (HttpServletResponse) response;
+			httpResponse.sendError(error);
+		}
+	}
+
+	public boolean isActionAllowed(ServletRequest request) {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		String command = httpRequest.getParameter("action");
+
+		if (command == null || command.isEmpty()) {
+			error = 404;
+			return false;
+		}
+		if (outOfControl.contains(command)) {
+			return true;
+		}
+		HttpSession session = httpRequest.getSession(false);
+		if (session == null) {
+			error = 403;
+			return false;
+		}
+		UserRole role = (UserRole) session.getAttribute("role");
+		if (role == null) {
+			error = 403;
+			return false;
+		}
+		if (accessMap.get(role).contains(command) || common.contains(command)) {
+			return true;
+		} else {
+			error = 403;
+			return false;
 		}
 	}
 
@@ -29,31 +61,12 @@ public class SecurityFilter implements Filter {
 		accessMap.put(UserRole.ADMIN, getParametersAsList(filterConfig.getInitParameter("adminCommands")));
 		accessMap.put(UserRole.USER, getParametersAsList(filterConfig.getInitParameter("userCommands")));
 		outOfControl = getParametersAsList(filterConfig.getInitParameter("outOfControlCommands"));
+		error = 0;
 	}
 
 	@Override
 	public void destroy() {
 		Filter.super.destroy();
-	}
-
-	public boolean isAllowed(ServletRequest request) {
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		String command = httpRequest.getParameter("action");
-		if (command == null || command.isEmpty()) {
-			return false;
-		}
-		if (outOfControl.contains(command)) {
-			return true;
-		}
-		HttpSession session = httpRequest.getSession(false);
-		if (session == null) {
-			return false;
-		}
-		UserRole role = (UserRole) session.getAttribute("role");
-		if (role == null) {
-			return false;
-		}
-		return accessMap.get(role).contains(command) || common.contains(command);
 	}
 
 	private List<String> getParametersAsList(String parameters) {
