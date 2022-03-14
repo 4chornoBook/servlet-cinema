1e77090b0f2ae2b394c38019ca5300dfb729b4c4
Kostiantyn Chorobuk <kostiantyn.chornobuk@gmail.com>
2022-03-14 20:44:12 +0200

    updated user

--- src/main/resources/cinemaUser.sql
+++ src/main/resources/cinemaUser.sql
@@ -1,2 +1,3 @@
 create user cinema_user with encrypted password 'user';
 grant all privileges on all tables in schema public TO cinema_user;
+grant usage, select on all sequences in schema public TO cinema_user;
