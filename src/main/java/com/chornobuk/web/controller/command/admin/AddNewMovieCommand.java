655f3c00af1df8cf4d6a2c1ce3e5b72b9c999433
Kostiantyn Chorobuk <kostiantyn.chornobuk@gmail.com>
2022-04-23 20:36:56 +0300

    added test for add new movie command

--- src/main/java/com/chornobuk/web/controller/command/admin/AddNewMovieCommand.java
+++ src/main/java/com/chornobuk/web/controller/command/admin/AddNewMovieCommand.java
@@ -18,6 +18,8 @@ import java.util.LinkedList;
 import java.util.stream.Collectors;
 
 public class AddNewMovieCommand implements ICommand {
+	MovieRepository movieRepository = new MovieRepository();
+	GenreRepository genreRepository = new GenreRepository();
 	@Override
 	public String execute(HttpServletRequest req, HttpServletResponse resp) {
 		Logger log = LogManager.getLogger(AddNewMovieCommand.class);
@@ -26,40 +28,40 @@ public class AddNewMovieCommand implements ICommand {
 		String forward = Path.ADD_NEW_MOVIE_PAGE;
 		String releaseDateString = req.getParameter("releaseDate");
 		LocalDate releaseDate = (releaseDateString == null || releaseDateString.isEmpty()) ? null : LocalDate.parse(releaseDateString);
-		int lengthInMinutes = Integer.parseInt(req.getParameter("length"));
+		int lengthInMinutes;
+		try {
+			lengthInMinutes = Integer.parseInt(req.getParameter("length"));
+		}
+		catch (NumberFormatException e) {
+			lengthInMinutes = -1;
+		}
 		String[] genresId = req.getParameterValues("genre");
 		String imageURL = req.getParameter("imageURL");
 		String description = req.getParameter("description");
-//		movie name min 1 max 200
+
 		if (name == null || name.isEmpty() || name.length() > 200) {
 			req.setAttribute("movieNameError", errorTag);
 		}
-//		add release date not greater than now
-//		|| releaseDate.isAfter(LocalDate.now()) this don't need because cinema
-//		can add session for future movies
 		else if (releaseDate == null) {
 			req.setAttribute("releaseDateError", errorTag);
 		}
-//		length min 20 max 600
 		else if (lengthInMinutes < 20 || lengthInMinutes > 600) {
 			req.setAttribute("lengthError", errorTag);
 		}
-//		add minimal genre
+//		at least one genre
 		else if (genresId == null || genresId.length == 0) {
 			req.setAttribute("genresError", errorTag);
 		}
-//		image url not null (optional check is it url)
 		else if (imageURL == null || imageURL.isEmpty()) {
 			req.setAttribute("imageURLError", errorTag);
 		}
-//		description not null
 		else if (description == null || description.isEmpty()) {
 			req.setAttribute("descriptionError", errorTag);
 		} else {
+			log.debug("fields are valid");
 			forward = Path.REDIRECT_COMMAND;
 			Movie movie = new Movie();
-			MovieRepository movieRepository = new MovieRepository();
-			GenreRepository genreRepository = new GenreRepository();
+
 			LinkedList<Genre> genres = genreRepository.getAll()
 					.stream()
 					.filter(x -> Arrays.binarySearch(genresId, x.getName()) >= 0)
---
+++ src/test/java/com/chornobuk/web/controller/command/admin/AddNewMovieCommandTest.java
@@ -0,0 +1,174 @@
+package com.chornobuk.web.controller.command.admin;
+
+import com.chornobuk.web.controller.Path;
+import com.chornobuk.web.model.entity.Genre;
+import com.chornobuk.web.model.entity.Movie;
+import com.chornobuk.web.model.repository.implementation.GenreRepository;
+import com.chornobuk.web.model.repository.implementation.MovieRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.time.LocalDate;
+import java.util.LinkedList;
+
+import static org.junit.Assert.*;
+
+public class AddNewMovieCommandTest {
+
+	@Mock
+	MovieRepository movieRepository;
+
+	@Mock
+	GenreRepository genreRepository;
+
+	@Mock
+	HttpServletRequest request;
+
+	@Mock
+	HttpServletResponse response;
+
+	@InjectMocks
+	AddNewMovieCommand command;
+
+	Movie testMovie;
+
+	@Before
+	public void setUp() throws Exception {
+		testMovie = new Movie();
+		testMovie.setName("testName");
+		testMovie.setReleaseDate(LocalDate.now());
+		testMovie.setImageURL("testURL");
+		testMovie.setLengthInMinutes(120);
+		testMovie.setDescription("test description");
+		LinkedList<Genre> genres = new LinkedList<>();
+		genres.add(new Genre(1, "testGenre"));
+		genres.add(new Genre(2, "testGenre2"));
+		testMovie.setGenres(genres);
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	public void successfulAdding () throws Exception{
+		Mockito.when(request.getParameter("name")).thenReturn(testMovie.getName());
+		Mockito.when(request.getParameter("releaseDate")).thenReturn(testMovie.getReleaseDate().toString());
+		Mockito.when(request.getParameter("length")).thenReturn(testMovie.getLengthInMinutes()+"");
+		Mockito.when(request.getParameter("imageURL")).thenReturn(testMovie.getImageURL());
+		Mockito.when(request.getParameter("description")).thenReturn(testMovie.getDescription());
+		Mockito.when(request.getParameterValues("genre")).thenReturn(testMovie.getGenres().stream().map(x->x.getId()+"").toArray(String[]::new));
+		Mockito.doNothing().when(movieRepository).add(Mockito.isA(Movie.class));
+		Mockito.when(genreRepository.getAll()).thenReturn(testMovie.getGenres());
+		command.execute(request,response);
+		Mockito.verify(response, Mockito.times(1)).sendRedirect(Path.INDEX_PAGE);
+	}
+
+
+	@Test
+	public void failedAllFieldsAreNotValid() throws Exception{
+		Mockito.when(request.getParameter("name")).thenReturn(null);
+		Mockito.when(request.getParameter("releaseDate")).thenReturn("");
+		Mockito.when(request.getParameter("length")).thenReturn(null);
+		Mockito.when(request.getParameter("imageURL")).thenReturn("");
+		Mockito.when(request.getParameter("description")).thenReturn("");
+		Mockito.when(request.getParameterValues("genre")).thenReturn(new String[]{});
+		Mockito.doNothing().when(movieRepository).add(Mockito.isA(Movie.class));
+		Mockito.when(genreRepository.getAll()).thenReturn(testMovie.getGenres());
+		String result = command.execute(request,response);
+		assertEquals(Path.ADD_NEW_MOVIE_PAGE, result);
+	}
+
+	@Test
+	public void failedNotValidName() throws Exception{
+		Mockito.when(request.getParameter("name")).thenReturn(null);
+		Mockito.when(request.getParameter("releaseDate")).thenReturn(testMovie.getReleaseDate().toString());
+		Mockito.when(request.getParameter("length")).thenReturn(testMovie.getLengthInMinutes()+"");
+		Mockito.when(request.getParameter("imageURL")).thenReturn(testMovie.getImageURL());
+		Mockito.when(request.getParameter("description")).thenReturn(testMovie.getDescription());
+		Mockito.when(request.getParameterValues("genre")).thenReturn(testMovie.getGenres().stream().map(x->x.getId()+"").toArray(String[]::new));
+		Mockito.doNothing().when(movieRepository).add(Mockito.isA(Movie.class));
+		Mockito.when(genreRepository.getAll()).thenReturn(testMovie.getGenres());
+		String result = command.execute(request,response);
+		assertEquals(Path.ADD_NEW_MOVIE_PAGE, result);
+	}
+
+	@Test
+	public void failedNotValidReleaseDate() throws Exception{
+		Mockito.when(request.getParameter("name")).thenReturn(testMovie.getName());
+		Mockito.when(request.getParameter("releaseDate")).thenReturn(null);
+		Mockito.when(request.getParameter("length")).thenReturn(testMovie.getLengthInMinutes()+"");
+		Mockito.when(request.getParameter("imageURL")).thenReturn(testMovie.getImageURL());
+		Mockito.when(request.getParameter("description")).thenReturn(testMovie.getDescription());
+		Mockito.when(request.getParameterValues("genre")).thenReturn(testMovie.getGenres().stream().map(x->x.getId()+"").toArray(String[]::new));
+		Mockito.doNothing().when(movieRepository).add(Mockito.isA(Movie.class));
+		Mockito.when(genreRepository.getAll()).thenReturn(testMovie.getGenres());
+		command.execute(request,response);
+		String result = command.execute(request,response);
+		assertEquals(Path.ADD_NEW_MOVIE_PAGE, result);
+	}
+
+	@Test
+	public void failedNotValidLength() throws Exception{
+		Mockito.when(request.getParameter("name")).thenReturn(testMovie.getName());
+		Mockito.when(request.getParameter("releaseDate")).thenReturn(testMovie.getReleaseDate().toString());
+		Mockito.when(request.getParameter("length")).thenReturn(null);
+		Mockito.when(request.getParameter("imageURL")).thenReturn(testMovie.getImageURL());
+		Mockito.when(request.getParameter("description")).thenReturn(testMovie.getDescription());
+		Mockito.when(request.getParameterValues("genre")).thenReturn(testMovie.getGenres().stream().map(x->x.getId()+"").toArray(String[]::new));
+		Mockito.doNothing().when(movieRepository).add(Mockito.isA(Movie.class));
+		Mockito.when(genreRepository.getAll()).thenReturn(testMovie.getGenres());
+		command.execute(request,response);
+		String result = command.execute(request,response);
+		assertEquals(Path.ADD_NEW_MOVIE_PAGE, result);
+	}
+
+	@Test
+	public void failedNotValidImageUrl() throws Exception{
+		Mockito.when(request.getParameter("name")).thenReturn(testMovie.getName());
+		Mockito.when(request.getParameter("releaseDate")).thenReturn(testMovie.getReleaseDate().toString());
+		Mockito.when(request.getParameter("length")).thenReturn(testMovie.getLengthInMinutes()+"");
+		Mockito.when(request.getParameter("imageURL")).thenReturn(null);
+		Mockito.when(request.getParameter("description")).thenReturn(testMovie.getDescription());
+		Mockito.when(request.getParameterValues("genre")).thenReturn(testMovie.getGenres().stream().map(x->x.getId()+"").toArray(String[]::new));
+		Mockito.doNothing().when(movieRepository).add(Mockito.isA(Movie.class));
+		Mockito.when(genreRepository.getAll()).thenReturn(testMovie.getGenres());
+		command.execute(request,response);
+		String result = command.execute(request,response);
+		assertEquals(Path.ADD_NEW_MOVIE_PAGE, result);
+	}
+
+	@Test
+	public void failedNotValidDescription() throws Exception{
+		Mockito.when(request.getParameter("name")).thenReturn(testMovie.getName());
+		Mockito.when(request.getParameter("releaseDate")).thenReturn(testMovie.getReleaseDate().toString());
+		Mockito.when(request.getParameter("length")).thenReturn(testMovie.getLengthInMinutes()+"");
+		Mockito.when(request.getParameter("imageURL")).thenReturn(testMovie.getImageURL());
+		Mockito.when(request.getParameter("description")).thenReturn(null);
+		Mockito.when(request.getParameterValues("genre")).thenReturn(testMovie.getGenres().stream().map(x->x.getId()+"").toArray(String[]::new));
+		Mockito.doNothing().when(movieRepository).add(Mockito.isA(Movie.class));
+		Mockito.when(genreRepository.getAll()).thenReturn(testMovie.getGenres());
+		command.execute(request,response);
+		String result = command.execute(request,response);
+		assertEquals(Path.ADD_NEW_MOVIE_PAGE, result);
+	}
+
+	@Test
+	public void failedNotValidGenres() throws Exception{
+		Mockito.when(request.getParameter("name")).thenReturn(testMovie.getName());
+		Mockito.when(request.getParameter("releaseDate")).thenReturn(testMovie.getReleaseDate().toString());
+		Mockito.when(request.getParameter("length")).thenReturn(testMovie.getLengthInMinutes()+"");
+		Mockito.when(request.getParameter("imageURL")).thenReturn(testMovie.getImageURL());
+		Mockito.when(request.getParameter("description")).thenReturn(testMovie.getDescription());
+		Mockito.when(request.getParameterValues("genre")).thenReturn(null);
+		Mockito.doNothing().when(movieRepository).add(Mockito.isA(Movie.class));
+		Mockito.when(genreRepository.getAll()).thenReturn(testMovie.getGenres());
+		command.execute(request,response);
+		String result = command.execute(request,response);
+		assertEquals(Path.ADD_NEW_MOVIE_PAGE, result);
+	}
+}
